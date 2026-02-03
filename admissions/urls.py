dc66c5d9522651216cfdbb5a73384ef0a2d66d4e
Disha <dishag325@gmail.com>
2026-02-03 09:52:54 +0530

    Fix form submit, success page and urls

--- admissions/urls.py
+++ admissions/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.admission_form, name='admission_form'),
-    path('success/', views.success, name='success'),
+    path('success/<int:pk>/', views.success, name='success'),
     path('pdf/<int:pk>/', views.admission_pdf, name='admission_pdf'),
 ]
--- admissions/views.py
+++ admissions/views.py
@@ -1,39 +1,24 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import AdmissionForm
 from .models import Admission
-from django.http import HttpResponse
-from django.template.loader import get_template
-from xhtml2pdf import pisa
 
-
-# Admission form page
 def admission_form(request):
     if request.method == 'POST':
         form = AdmissionForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect('success')
+            admission = form.save()
+            return redirect('success', pk=admission.pk)
     else:
         form = AdmissionForm()
 
     return render(request, 'admissions/admission_form.html', {'form': form})
 
 
-# Success page
-def success(request):
-    return render(request, 'admissions/success.html')
+def success(request, pk):
+    admission = get_object_or_404(Admission, pk=pk)
+    return render(request, 'admissions/success.html', {'admission': admission})
 
 
-# PDF view
 def admission_pdf(request, pk):
     admission = get_object_or_404(Admission, pk=pk)
-
-    template = get_template('admissions/pdf.html')
-    html = template.render({'admission': admission})
-
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'inline; filename="admission.pdf"'
-
-    pisa.CreatePDF(html, dest=response)
-
-    return response
+    return render(request, 'admissions/pdf.html', {'admission': admission})
