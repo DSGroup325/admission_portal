f671ba70f9bef1c3fb73de79118b687500141785
Disha <dishag325@gmail.com>
2026-02-09 15:51:20 +0530

    Fixed DOB date format using date picker

--- admissions/forms.py
+++ admissions/forms.py
@@ -1,12 +1,14 @@
 from django import forms
 from .models import Admission
 
-
 class AdmissionForm(forms.ModelForm):
     class Meta:
         model = Admission
-        fields = '__all__'
-
+        fields = "__all__"
         widgets = {
-            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
+            'dob': forms.DateInput(
+                attrs={'type': 'date'}
+            )
+        }
+
         }
